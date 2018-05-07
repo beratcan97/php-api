f1696bfbd1d378b9dc31f3648550b6df83bd712d
Beratcan Acil <beratcan_acil@hotmail.se>
2018-05-07 10:29:44 +0200

    UserController

---
+++ src/App/Controllers/UsersController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controllers;
+
+class UsersController
+{
+    private $db;
+
+    /**
+     * Dependeny Injection (DI): http://www.phptherightway.com/#dependency_injection
+     * If this class relies on a database-connection via PDO we inject that connection
+     * into the class at start. If we do this TodoController will be able to easily
+     * reference the PDO with '$this->db' in ALL functions INSIDE the class
+     * This class is later being injected into our container inside of 'App/container.php'
+     * This results in we being able to call '$this->get('Todos')' to call this class
+     * inside of our routes.
+     */
+    public function __construct(\PDO $pdo)
+    {
+        $this->db = $pdo;
+    }
+
+    public function getAll()
+    {
+        $getAll = $this->db->prepare('SELECT * FROM users');
+        $getAll->execute();
+        return $getAll->fetchAll();
+    }
+
+    public function getOne($id)
+    {
+        $getOne = $this->db->prepare('SELECT * FROM users WHERE id = :id');
+        $getOne->execute([':id' => $id]);
+        return $getOne->fetch();
+    }
+
+    public function add($username ,$password, $createdAt)
+    {
+        /**
+         * Default 'completed' is false so we only need to insert the 'content'
+         */
+        $addOne = $this->db->prepare(
+            'INSERT INTO users (username, password, createdAt) VALUES (:username, :password, :createdAt)'
+        );
+        /**
+         * Insert the value from the parameter into the database
+         */
+        $addOne->execute(
+            //ex [':content'  => $todo['content']]
+            [':username'  => $username,
+            ':password'  => $password,
+            ':createdAt'  => $createdAt]
+        );
+    }
+}
