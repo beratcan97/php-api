63732515437be0facd652572d2bf8a3494eaf538
maia <medeea@rocketmail.com>
2018-05-09 11:11:39 +0200

    navbar & other fixes

--- src/App/ConfigHandler.php
+++ src/App/ConfigHandler.php
@@ -29,9 +29,9 @@ class ConfigHandler
         $config = [];
         $config['displayErrorDetails'] = true;
         $config['addContentLengthHeader'] = false;
-        $config['db']['host']   = 'localhost';
+        $config['db']['host']   = 'localhost:8889';
         $config['db']['user']   = 'root';
-        $config['db']['pass']   = '';
+        $config['db']['pass']   = 'root';
         $config['db']['dbname'] = 'phpapi';
         return $config;
     }
