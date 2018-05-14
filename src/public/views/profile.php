b4dfe764e1e80512160eb8494850bb17895664f2
Beratcan Acil <beratcan_acil@hotmail.se>
2018-05-14 11:33:38 +0200

    profile

---
+++ src/public/views/profile.php
@@ -0,0 +1,22 @@
+<?php require_once('components/head.php'); ?>
+
+<body>
+  <?php require_once('components/navbar.php'); ?>
+  <main class="container">
+		<?php if (!isset($_SESSION['loggedIn'])): ?>
+			<div class="message-body">
+				<h1 class="title">Welcome!</h1>
+				<h2 class="subtitle">Please sign in or register a new account!</h2>
+			</div>
+		<?php else:
+?>
+  <p class="title">Welcome, <?= $_SESSION['username'] ?> </p>
+<?php endif;
+  include_once('components/entry_form.php');
+?>
+  <div id="entries_container" class="entries_container">
+
+  </div>
+</main>
+
+<?php require_once('components/footer.php'); ?>
