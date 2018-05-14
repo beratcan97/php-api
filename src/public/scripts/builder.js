fc69ec661b65887e631ec60fe1d95559abe1b6c4
Leo <leolainen2@gmail.com>
2018-05-14 12:29:45 +0200

    added some stuff to entries

--- src/public/scripts/builder.js
+++ src/public/scripts/builder.js
@@ -15,20 +15,28 @@ const builder = (function() {
 
     let entryWrapper = create.elem("div");
     let title = create.elem("h1");
+    let createdBy = create.elem("h2");
+    let date = create.elem("span");
     let content = create.elem("p");
     let commentSpan = create.elem("span");
 
     let titleText = create.text(entry.title);
     let contentText = create.text(entry.content);
+    let dateText = create.text(entry.createdAt);
+    let createdByText = create.text("createdBy id: " + entry.createdBy);
     let commentSpanText = create.text(amountOfComments + " comments");
 
     entryWrapper.classList.add("entries_wrapper");
 
     title.appendChild(titleText);
     content.appendChild(contentText);
+    date.appendChild(dateText);
     commentSpan.appendChild(commentSpanText);
+    createdBy.appendChild(createdByText);
     entryWrapper.appendChild(title);
+    entryWrapper.appendChild(createdBy);
     entryWrapper.appendChild(content);
+    entryWrapper.appendChild(date);
     entryWrapper.appendChild(commentSpan);
 
     return entryWrapper;
