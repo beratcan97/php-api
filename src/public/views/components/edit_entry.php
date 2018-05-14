3e1731f54e2b5703e896d382d2e334bcfb871ce7
maia <medeea@rocketmail.com>
2018-05-14 11:15:54 +0200

    edit_entry

--- src/public/views/components/edit_entry.php
+++ src/public/views/components/edit_entry.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<?php require_once('partials/head.php'); ?>
+	<?php require_once('components/head.php'); ?>
 </head>
 <body>
-	<?php require_once('partials/navbar.php'); ?>
+	<?php require_once('components/navbar.php'); ?>
 	
     <div class="container">
         <?php if(isset($_SESSION['userID'])): ?>
