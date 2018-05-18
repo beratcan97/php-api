8e77a03936ac17fb96154cd6a502940831a7c3c1
maia <medeea@rocketmail.com>
2018-05-18 11:49:09 +0200

    style fixes

--- src/public/views/components/register.php
+++ src/public/views/components/register.php
@@ -23,7 +23,7 @@
           </span>
       </div>
       <div class="control">
-        <button type="submit" name="registerBtn" id="registerBtn" class="button is-outlined is-primary">Register</button>
+        <button type="submit" name="registerBtn" id="registerBtn" class="button is-outlined is-primary btn-style">Register</button>
       </div>
     </div>
   </form>
--- src/public/views/index.php
+++ src/public/views/index.php
@@ -16,29 +16,6 @@
       </div>
     </div>
     </section>
-
-
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">
-          Title
-        </p>
-        <button type="submit" name="editBtn" id="editBtn" class="button is-outlined is-primary has-margin">Edit</button>
-        <button type="submit" name="deleteBtn" id="deleteBtn" class="button is-outlined is-danger">Delete</button>
-      </header>
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-          <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-          <br>
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div>
-      </div>
-      <footer class="card-footer is-right">
-        <button type="submit" name="likeBtn" id="likeBtn" class="button is-outlined is-info">Likes</button>
-        <button type="submit" name="commentBtn" id="commentBtn" class="button is-outlined is-info">Comments</button>
-      </footer>
-    </div>
    
 		<?php else: ?>
       <p class="title">Welcome, <?= $_SESSION['username'] ?> </p>
