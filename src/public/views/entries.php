0a6540c8776fd07a62d4fe746373272dd8666d95
Leo <leolainen2@gmail.com>
2018-05-21 12:08:06 +0200

    removed redundant hero/header thingy in entries.php

--- src/public/views/entries.php
+++ src/public/views/entries.php
@@ -7,16 +7,6 @@ endif; ?>
 <body>
   <?php require_once('components/navbar.php'); ?>
   <main class="container">
-    <section class="hero is-medium is-primary is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <div class="card">
-            <p>Log in if you want to see anything, loser!</p>
-          </div>
-        </div>
-      </div>
-    </section>
-
       <span id="search_wrapper" class="searchbar-style">
         <input type="text" class="input" name="searchbar" placeholder="Search entries by title" id="searchbar">
       </span>
