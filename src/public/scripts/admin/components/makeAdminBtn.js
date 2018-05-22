0c4af5b24530c41b0d7f02590588421c49c07a8a
Beratcan Acil <beratcan_acil@hotmail.se>
2018-05-22 10:13:23 +0200

    button text

--- src/public/scripts/admin/components/makeAdminBtn.js
+++ src/public/scripts/admin/components/makeAdminBtn.js
@@ -1,16 +1,16 @@
 import { create } from '../../utils';
 
 export function makeAdminBtn(user) {
-  let admin = false;
+  let admin = "Ej admin";
   const adminBtn = create.elem('button');
 
   const clsLC = ['button', 'is-outlined', 'is-info', 'is-small'];
   adminBtn.classList.add(...clsLC);
 
   if (user.admin === 0) {
-    admin = false;
+    admin = "Ej admin";
   } else {
-    admin = true;
+    admin = "Admin";
   }
 
   const adminBtnText = create.text(admin);
