c80aed67de8b63191d3c1846c6523bf5791ee2eb
Leo <leolainen2@gmail.com>
2018-05-23 11:32:56 +0200

    cleaned and sorted code in deleteBtn.js

--- src/public/scripts/components/entries/partials/deleteBtn.js
+++ src/public/scripts/components/entries/partials/deleteBtn.js
@@ -3,14 +3,14 @@ import { create } from '../../../modules/utils';
 
 export async function DeleteBtn(entry) {
   const deleteBtn = create.elem('button');
+  const deleteIcon = create.elem('i');
 
   const clsD = ['button', 'is-outlined', 'is-danger'];
-  deleteBtn.classList.add(...clsD);
+  const clsDI = ['fas', 'fa-trash-alt', 'icons'];
 
-  const deleteIcon = create.elem('i');
-  deleteIcon.classList.add('fas');
-  deleteIcon.classList.add('fa-trash-alt');
-  deleteIcon.classList.add('icons');
+  deleteBtn.classList.add(...clsD);
+  deleteIcon.classList.add(...clsDI);
+
   deleteBtn.appendChild(deleteIcon);
 
   deleteBtn.onclick = function() {
